485f7b69026e0cfeb7b3ee7515da6b3ef42ecd8d
Kyubo Sim <70837945+SyaDo95@users.noreply.github.com>
2024-10-11 13:22:48 +0900

    Title: 중앙값 구하기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 241011/중앙값 구하기/find-the-median.java
+++ 241011/중앙값 구하기/find-the-median.java
@@ -3,35 +3,16 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        int b = s.nextInt();
-        int c = s.nextInt();
-
         int [] num = new int [3];
-        num[0] = a;
-        if(a < b){
-            if(b<c){
-                System.out.println(b);
-            }
-            if(c < b){
-                System.out.println(c);
-            }
-        }
-        if(b < a){
-            num[0] = b;
-            if(c < b){
-                System.out.println(b);
-            }
-            else if(a < c){
-                System.out.println(a);
-            }
-        }else if(c < a){
-            System.out.println(a);
-        }else if(c<b){
-            System.out.println(c);
-        }else if(b<c){
-            System.out.println(b);
-        }
+        num[0] = s.nextInt();
+        num[1] = s.nextInt();
+        num[2] = s.nextInt();
+        Arrays.sort(num);
         
+        System.out.println(num[1]);
+
+
+
+
     }
 }
