1ebb0d16d6a3962a32dffb45ae9fe90e3bd260df
Kyubo Sim <70837945+SyaDo95@users.noreply.github.com>
2024-10-11 11:39:55 +0900

    Title: 4번의 크기 비교, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241011/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        int d = s.nextInt();
+        int e = s.nextInt();
+
+        if(a>b){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+        if(a>c){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+        if(a>d){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+        if(a>e){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+    }
+}
