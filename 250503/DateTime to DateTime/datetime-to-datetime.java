5c487e334256d001f392786472a73686d88f80e3
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-05-03 00:14:20 +0900

    Title: DateTime to DateTime, Time: 116ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250503/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static int dayToMin(int x) {
+        return x * 24 * 60;
+    }
+    public static int hourToMin(int x) {
+        return x * 60;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        // Please write your code here.
+
+        int startMin = dayToMin(11)+hourToMin(11)+11;
+        int endMin = dayToMin(A)+hourToMin(B)+C;
+
+        System.out.print(endMin-startMin);
+    }
+}
