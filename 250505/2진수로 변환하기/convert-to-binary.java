39c79246fdcae502d33f4dd62fd20cab0baf28b3
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-05-05 17:09:58 +0900

    Title: 2진수로 변환하기, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

--- 250505/2진수로 변환하기/convert-to-binary.java
+++ 250505/2진수로 변환하기/convert-to-binary.java
@@ -6,6 +6,9 @@ public class Main {
         // Please write your code here.
         int i = 0;
         int[] two = new int[20];
+        if( n == 0){
+            System.out.print("0");
+        }
         while(n > 0){
             two[i] = n % 2;
             n = n / 2; 
