35a5f1a46211740c33cefdeba5a6ff6531d0c368
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-05-11 07:02:54 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 152ms, Memory: 30MB, Status: Runtime Error - Codetree

---
+++ 250511/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+    public static final int Max = 2001;
+    public static int[][] checked = new int[Max][Max];
+    public static int count = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ax1 = sc.nextInt();
+        int ay1 = sc.nextInt();
+        int ax2 = sc.nextInt();
+        int ay2 = sc.nextInt();
+        int bx1 = sc.nextInt();
+        int by1 = sc.nextInt();
+        int bx2 = sc.nextInt();
+        int by2 = sc.nextInt();
+        int mx1 = sc.nextInt();
+        int my1 = sc.nextInt();
+        int mx2 = sc.nextInt();
+        int my2 = sc.nextInt();
+        // Please write your code here.
+        for(int i = ax1; i <ax2; i++){
+            for(int j = ay1; j < ay2; j++){
+                checked[i][j] = 1;
+            }
+        }
+        for(int i = bx1; i <bx2; i++){
+            for(int j = by1; j < by2; j++){
+                checked[i][j] = 1;
+            }
+        }
+        for(int i = mx1; i <mx2; i++){
+            for(int j = my1; j < my2; j++){
+                checked[i][j] = 0;
+            }
+        }
+        for(int i = 0; i <Max; i++){
+            for(int j = 0; j < Max; j++){
+                if(checked[i][j] == 1){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
