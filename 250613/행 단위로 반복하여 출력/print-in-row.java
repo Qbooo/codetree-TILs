908169421729b6e274aec0326d1fb11c4c630e32
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-06-13 15:49:43 +0900

    Title: 행 단위로 반복하여 출력, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250613/행 단위로 반복하여 출력/print-in-row.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
