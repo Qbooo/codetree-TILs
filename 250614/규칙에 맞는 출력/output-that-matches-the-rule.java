1fae0da0d92628189e19a48bbb688818a0037f5e
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-06-14 15:27:13 +0900

    Title: 규칙에 맞는 출력, Time: 141ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250614/규칙에 맞는 출력/output-that-matches-the-rule.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for(int i = n; i > 0; i--){
+            for(int j = i; j < n; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
