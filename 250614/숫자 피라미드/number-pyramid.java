09677d0578ac29e8efd8affd79733c0c4f6c8339
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-06-14 15:03:57 +0900

    Title: 숫자 피라미드, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

--- 250614/숫자 피라미드/number-pyramid.java
+++ 250614/숫자 피라미드/number-pyramid.java
@@ -9,7 +9,7 @@ public class Main {
             for(int j = 0; j < i; j++){
                 System.out.print(i + " ");
             }
-            System.out.println()
+            System.out.println();
         }
 
     }
