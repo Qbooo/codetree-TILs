f1acce08c95a24a3c1cdd081ace19aaf17908fbc
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-06-14 15:28:11 +0900

    Title: 규칙에 맞는 출력, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 250614/규칙에 맞는 출력/output-that-matches-the-rule.java
+++ 250614/규칙에 맞는 출력/output-that-matches-the-rule.java
@@ -6,7 +6,7 @@ public class Main {
         int n = s.nextInt();
 
         for(int i = n; i > 0; i--){
-            for(int j = i; j < n; j++){
+            for(int j = i; j <= n; j++){
                 System.out.print(j+" ");
             }
             System.out.println();
