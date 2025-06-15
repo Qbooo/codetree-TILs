af68330bcfc1a23b135a4160e26c21ce30bbc3d4
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-06-15 13:54:26 +0900

    Title: 완전수, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

--- 250615/완전수/perfect-number.java
+++ 250615/완전수/perfect-number.java
@@ -9,15 +9,19 @@ public class Main {
         int cnt = 0;
 
         for(int i = start; i <= end; i++){
+            sum = 0;
             for(int j = 1; j < i; j++){
                 if(i%j == 0){
                     sum = sum + j;
                 }
             }
+            
             if(sum == i){
                 cnt++;
             }
+            
         }
+        
         System.out.print(cnt);
 
 
