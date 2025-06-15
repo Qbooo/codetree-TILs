381dd56b1ecc99304e6533a83a7e3d0a349479d6
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-06-15 13:22:37 +0900

    Title: 알파벳 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250615/알파벳 출력하기/print-alphabet.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int num = 65;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print((char)num);
+                num++;
+                if(num == 91){
+                    num = 65;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
