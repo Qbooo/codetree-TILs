05e60da60e61d57bf69263cdb5a317dce0acfaa2
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-06-15 15:22:31 +0900

    Title: 특정 수 도달하기, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250615/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+        int n = 0;
+        int sum = 0;
+        double avg = 0;
+        int cnt = 0;
+        for(int i = 0; i < 10; i++){
+            n = s.nextInt();
+            if(n >= 250){
+                break;
+            }
+            sum = sum + n;
+            cnt++;
+        }
+        System.out.print(sum+" "+(double)sum/cnt);
+    }
+}
