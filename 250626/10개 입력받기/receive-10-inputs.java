4e1e512e207dfd576f1e1c4384022538ce4dc09c
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-06-26 14:49:05 +0900

    Title: 10개 입력받기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250626/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+
+        int cnt = 0;
+        double sum = 0;
+        
+        while(cnt < 10){
+            int n = s.nextInt();
+            if(n == 0){
+                break;
+            }
+            sum += n;
+            cnt++;
+        }
+        
+        System.out.println((int)sum+" "+sum/cnt);
+
+
+    }
+}
