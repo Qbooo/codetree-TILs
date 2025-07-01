fbc1bdb111d4d80780ebc3b043bf6c0bde12f330
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-01 15:09:31 +0900

    Title: 특정 위치의 문자 2, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250701/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+        String line = s.nextLine();
+        String[] part = line.split(" ");
+
+
+      
+
+        System.out.println(part[1]+" "+part[4]+" "+part[7]);
+
+    }
+}
