2f0130674a909b0fc0b48dfb0077e959c47ead51
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-02 17:14:36 +0900

    Title: 전항의 두 배, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250702/전항의 두 배/twice-the-previous.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+        int[] num = new int[10];
+        num[0] = s.nextInt();
+        num[1] = s.nextInt();
+
+        for(int i = 0; i < 8; i++){
+            num[i+2] = num[i+1] + 2*num[i];
+        }
+
+        for(int i = 0; i < 10; i++){
+            System.out.print(num[i]+" ");
+        }
+
+    }
+}
