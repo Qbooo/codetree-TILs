7236708494cde945d5d189428b445694ba2a0ba8
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-24 18:55:01 +0900

    Title: 문자열의 길이 출력하기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250724/문자열의 길이 출력하기/print-strings-length.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+
+        String n = s.next();
+        String m = s.next();
+
+        System.out.println(n.length()+m.length());
+
+    }
+}
