33eb4bf2d328aad7c1b77f80ccfca7b1712aa5eb
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-24 18:52:13 +0900

    Title: 문자열 두번 출력하기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250724/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+        String n = s.next();
+
+        System.out.println(n);
+         System.out.println(n);
+    }
+}
