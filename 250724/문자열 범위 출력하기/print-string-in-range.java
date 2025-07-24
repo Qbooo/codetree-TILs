8fa1c3a5e28b78d9f31ba3c3c08fe09aed7bf75b
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-24 19:09:03 +0900

    Title: 문자열 범위 출력하기, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250724/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+
+        String w = s.nextLine();
+
+        for(int i = 2; i < 10; i++){
+            System.out.print(w.charAt(i));
+        }
+
+    }
+}
