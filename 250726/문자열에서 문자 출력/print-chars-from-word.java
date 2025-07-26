41d00ebe13e6338bb0841c7b29e09f5824cc0c7c
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-26 15:33:03 +0900

    Title: 문자열에서 문자 출력, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250726/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+
+        String w = s.nextLine();
+
+        for(int i = 0; i < w.length(); i++){
+            System.out.println(w.charAt(i));
+        }
+
+    }
+}
