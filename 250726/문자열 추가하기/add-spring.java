2595db4ed47ce0659eef44389d1ee47df089756f
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-26 16:26:21 +0900

    Title: 문자열 추가하기, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250726/문자열 추가하기/add-spring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+
+        String w = s.nextLine();
+
+        System.out.println(w+"Hello");
+
+    }
+}
