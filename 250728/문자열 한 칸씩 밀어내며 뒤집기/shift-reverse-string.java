10f91f684bf0af72f0fa061997274c3eb08bb021
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-28 19:59:51 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 296ms, Memory: 22MB, Status: Passed - Codetree

--- 250728/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 250728/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -1,30 +1,24 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner s = new Scanner(System.in);
 
         String w = s.next();
-
         int q = s.nextInt();
 
         for(int i = 0; i < q; i++){
             int n = s.nextInt();
             if(n == 1){
-                w = w.substring(1,w.length()) + w.substring(0,1);
+                w = w.substring(1) + w.substring(0,1);
                 System.out.println(w);
-            }else if(n == 2){
-                w = w.substring(w.length()-1, w.length()) + w.substring(0,w.length()-1);
+            } else if(n == 2){
+                w = w.substring(w.length()-1) + w.substring(0,w.length()-1);
                 System.out.println(w);
-            }else if(n == 3){
-                String r = "";
-                for(int j = w.length()-1; j >= 0; j--){
-                    r += w.charAt(j);
-                }
-                w = r;
+            } else if(n == 3){
+                w = new StringBuilder(w).reverse().toString(); // ✅ 이 부분만 변경
                 System.out.println(w);
             }
         }
-
     }
 }
