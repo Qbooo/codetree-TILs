d6012e29bb50de94ccc7e1042b18ad8e9d625e29
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-28 13:24:04 +0900

    Title: 부분문자열 위치 구하기, Time: 116ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250728/부분문자열 위치 구하기/find-location-of-substring.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+
+        String aa = s.nextLine();
+        String bb = s.nextLine();
+
+        boolean ex = false;
+        int index = 0;
+
+        for(int i = 0; i < aa.length() - bb.length(); i++){
+            if(aa.substring(i,i+bb.length()).equals(bb)){
+                System.out.print(i);
+                ex = true;
+                break;
+            }
+        }
+
+        if(ex == false){
+            System.out.println("-1");
+        }
+
+    }
+}
