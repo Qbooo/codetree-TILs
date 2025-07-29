d5bfa3efa7919462560d336d21c4718f2d3b3c47
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-29 13:43:00 +0900

    Title: 아스키코드표 맞추기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250729/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+
+        for(int i = 0; i < 5; i++){
+            int n = s.nextInt();
+            System.out.print((char)n+" ");
+        }
+
+    }
+}
