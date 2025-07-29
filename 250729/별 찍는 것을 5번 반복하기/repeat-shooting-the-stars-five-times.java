8a9034c33399a0425759bfbb9a9c64a062424523
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-29 18:39:30 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250729/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void printstars(){
+        System.out.println("**********");
+    }
+    public static void main(String[] args) {
+        // Please write your code here.
+        for(int i = 0; i < 5; i++){
+            printstars();
+        }
+    }
+}
