e3a4ace35fd8b817fea7b8932f752fc53dedb8b7
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-29 13:44:59 +0900

    Title: 아스키코드 변환, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250729/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s = new Scanner(System.in);
+
+        char a = s.next().charAt(0);
+        int n = s.nextInt();
+
+        System.out.print((int)a +" "+ (char)n);
+
+    }
+}
