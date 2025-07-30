a4e21811ff4d395cbbc3264f81235f9ce21766f8
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-07-30 17:34:50 +0900

    Title: 함수를 이용한 윤년 판별, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250730/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isYoon(int y){
+        if(y%100 == 0 && y%400 != 0){
+            return false;
+        }
+        if(y%4 == 0){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        // Please write your code here.
+        System.out.print(isYoon(y));
+    }
+}
