abcd2920cd8f6ec9dcce127f90745a0e5c534021
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-08-04 18:21:04 +0900

    Title: 두 정수에 대한 연산값, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250804/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class Main {
+    public static void a(AtomicInteger x, AtomicInteger y){
+        if(x.get() >= y.get()){
+            x.set(x.get()+25);
+            y.set(y.get()*2);
+        }else{
+            x.set(x.get()*2);
+            y.set(y.get()+25);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+        AtomicInteger x = new AtomicInteger(a);
+        AtomicInteger y = new AtomicInteger(b);
+        a(x, y);
+        System.out.print(x.get()+" "+y.get());
+    }
+}
