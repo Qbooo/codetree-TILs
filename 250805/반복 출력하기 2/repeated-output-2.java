d27afc48b14ea2f6863b231f07a16d06163d03de
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-08-05 17:23:19 +0900

    Title: 반복 출력하기 2, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250805/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void hi(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println("HelloWorld");
+        hi(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        hi(n);
+    }
+}
