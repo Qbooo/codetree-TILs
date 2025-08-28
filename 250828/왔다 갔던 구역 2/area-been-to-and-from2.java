2ad20f100b7c50d4c7c6d975cb7f956ffd097279
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-08-28 23:15:43 +0900

    Title: 왔다 갔던 구역 2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250828/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] ss = new int[1001];
+        int m = 500;
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char dir = sc.next().charAt(0);
+            // Please write your code here.
+            if(dir == 'L'){
+                for(int j = m; j > m - x; j--){
+                    ss[j]++;
+                }
+            }else{
+                for(int j = m; j < m + x; j++){
+                    ss[j]++;
+                }
+            }
+        }
+        int cnt = 0;
+        for(int i = 0; i < 1001; i++){
+            if(ss[i] > 1){
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt - 1);
+    }
+}
