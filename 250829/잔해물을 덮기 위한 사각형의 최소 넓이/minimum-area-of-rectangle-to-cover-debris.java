283a8d866affaf2de006349d9d69041278d93743
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-08-29 22:37:03 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 220ms, Memory: 27MB, Status: Wrong Answer - Codetree

--- 250829/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 250829/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -21,7 +21,10 @@ public class Main {
         }else if((rect2_y1 > rect1_y1 && rect2_y1 < rect1_y2) && (rect2_y2 > rect1_y1 && rect2_y2 < rect1_y2)
         && (rect2_x1 < rect1_x1 && rect2_x2 > rect1_x2)){
             System.out.print((rect1_x2-rect1_x1) * (rect1_y2-rect1_y1));
-        }else{
+        }else if((rect2_x1 < rect1_x1 && rect2_y1 < rect1_y1) && (rect2_x2 > rect1_x2 && rect2_y2 > rect1_y2)){
+            System.out.print("0");
+        }
+        else{
             for(int i = rect1_x1 + 1000; i < rect1_x2 + 1000; i++){
             for(int j = rect1_y1 + 1000;  j < rect1_y2 + 1000; j++){
                 sq[i][j] = 1;
