8fc70a38779c170d9050cd3e660f446bb26ac990
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-08-29 19:57:19 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 245ms, Memory: 27MB, Status: Wrong Answer - Codetree

---
+++ 250829/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rect1_x1 = sc.nextInt();
+        int rect1_y1 = sc.nextInt();
+        int rect1_x2 = sc.nextInt();
+        int rect1_y2 = sc.nextInt();
+        int rect2_x1 = sc.nextInt();
+        int rect2_y1 = sc.nextInt();
+        int rect2_x2 = sc.nextInt();
+        int rect2_y2 = sc.nextInt();
+        // Please write your code here.
+
+        int[][] sq = new int[2001][2001];
+        for(int i = rect1_x1 + 1000; i < rect1_x2 + 1000; i++){
+            for(int j = rect1_y1 + 1000;  j < rect1_y2 + 1000; j++){
+                sq[i][j] = 1;
+            }
+        }
+        for(int i = rect2_x1 + 1000; i < rect2_x2 + 1000; i++){
+            for(int j = rect2_y1 + 1000;  j < rect2_y2 + 1000; j++){
+                sq[i][j] = 0;
+            }
+        }
+        int vMax = 0;
+        int hMax = 0;
+        for(int i = 0; i < 2001; i++){
+            int hCnt = 0;
+            int vCnt = 0;
+            for(int j = 1; j < 2001; j++){
+                if(sq[i][j-1] == 1 && sq[i][j] == 1){
+                    hCnt++;
+                }
+                if(sq[j-1][i] == 1 && sq[j][i] == 1){
+                    vCnt++;
+                }
+            }
+            if(hMax < hCnt){
+                hMax = hCnt;
+            }
+            if(vMax < vCnt){
+                vMax = vCnt;
+            }
+        }
+
+        System.out.print((hMax+1)*(vMax+1));
+
+    }
+}
