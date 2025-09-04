1390d1e40d4b5b6f77f50a1dbf618ab10623b651
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-09-04 19:13:23 +0900

    Title: 만나는 그 순간, Time: 126ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250904/만나는 그 순간/the-moment-we-meet.java
+++ 250904/만나는 그 순간/the-moment-we-meet.java
@@ -7,65 +7,55 @@ public class Main {
         int m = sc.nextInt();
         
         // Please write your code here.
-        int[] ax = new int[2000001];
-        int[] bx = new int[2000001];
 
-        ax[1000000] = 0;
-        bx[1000000] = 0;
-        int cnt = 0;
-        int x = 1000000;
+        int[] at = new int[1000001];
+        int[] bt = new int[1000001];
+
+        int t = 0;
+        int loca = 0;
         for(int i = 0; i < n; i++){
-            char a = sc.next().charAt(0);
-            int t = sc.nextInt();
-        
+            char l = sc.next().charAt(0);
+            int tt = sc.nextInt();
 
-            if(a == 'R'){
-                for(int j = x+1; j <= x + t; j++){
-                    ax[j] = ++cnt;
+            for(int j = t; j < t + tt; j++){
+                if(l == 'R'){
+                    at[j] = ++loca;
+                }else{
+                    at[j] = --loca;
                 }
-                x = x + t;
-            }else if(a == 'L'){
-                for(int j = x-1; j >= x - t; j--){
-                    ax[j] = ++cnt;
-                }
-                x = x - t;
             }
 
         }
-        cnt = 0;
-        x = 1000000;
+
+        t = 0;
+        loca = 0;
         for(int i = 0; i < m; i++){
-            char b = sc.next().charAt(0);
-            int t = sc.nextInt();
+            char l = sc.next().charAt(0);
+            int tt = sc.nextInt();
 
-            if(b == 'R'){
-                for(int j = x+1; j <= x + t; j++){
-                    bx[j] = ++cnt;
+            for(int j = t; j < t + tt; j++){
+                if(l == 'R'){
+                    bt[j] = ++loca;
+                }else{
+                    bt[j] = --loca;
                 }
-                x = x + t;
-            }else if(b == 'L'){
-                for(int j = x-1; j >= x - t; j--){
-                    bx[j] = ++cnt;
-                }
-                x = x - t;
             }
 
         }
+
         boolean meet = false;
-        int min = Integer.MAX_VALUE;
-        for(int i = 1; i < 2000001; i++){
-            //System.out.println("log =====> ax" + i + " " + ax[i] + " /// bx"+i+ " " + bx[i]);
-            if(ax[i] == bx[i] && ax[i] > 0){
+
+
+        for(int i = 0; i < 1000001; i++){
+            if(at[i] == bt[i]){
+                System.out.print(i);
                 meet = true;
-                if(min > ax[i]){
-                    min = ax[i];
-                }
+                break;
             }
         }
+
         if(meet == false){
             System.out.print("-1");
-        }else{
-            System.out.print(min);
         }
 
     }
