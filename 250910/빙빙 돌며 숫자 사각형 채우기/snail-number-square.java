69fb3f6ef934510c79226f1cd80fb03372d7e654
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-09-10 14:50:03 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250910/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static int[] dx = {0, 1, 0 ,-1};
+    public static int[] dy = {1, 0, -1, 0};
+    public static int x = 0, y = 0;
+    public static int dirNum = 0;
+
+    public static boolean inRange(int x, int y, int n, int m){
+        return (0 <= x && x < n && 0 <= y && y < m);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        // Please write your code here.
+        int[][] square = new int [n][m];
+
+        square[x][y] = 1;
+
+        for(int i = 2; i <= n * m; i++){
+            int nx = x + dx[dirNum], ny = y + dy[dirNum];
+
+            if(!inRange(nx, ny, n, m) || square[nx][ny] != 0){
+                dirNum = (dirNum + 1) % 4;
+            }
+
+            x = x + dx[dirNum]; y = y + dy[dirNum];
+            square[x][y] = i;
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(square[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
