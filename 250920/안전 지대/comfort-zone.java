c0afd81b42e8163ff368265e5e7a76b0952e9334
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-09-20 16:53:02 +0900

    Title: 안전 지대, Time: 295ms, Memory: 22MB, Status: Passed - Codetree

--- 250920/안전 지대/comfort-zone.java
+++ 250920/안전 지대/comfort-zone.java
@@ -25,7 +25,7 @@ public class Main {
     }
     public static void main(String[] args) {
         int maxSafe = 0;
-        int safeK = 0;
+        int safeK = 1;
 
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
