1ce65c18e2e34142cbc8bc35acec31aecea2fd3b
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-10-05 16:48:34 +0900

    Title: 거꾸로 순열, Time: 1155ms, Memory: 27MB, Status: Passed - Codetree

---
+++ 251005/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Main {
+    public static int n;
+    public static boolean[] visited = new boolean[9];
+    public static ArrayList<Integer> ans = new ArrayList<>();
+    public static void printNum(){
+
+            for(int a : ans){
+                System.out.print(a+" ");
+            }
+            System.out.println();
+        
+    }
+    public static void choose(int currNum){
+        if(currNum == n){
+            printNum();
+            return;
+        }
+
+        for(int i = n; i > 0; i--){
+            if(visited[i]){
+                continue;
+            }else{
+                visited[i] = true;
+                ans.add(i);
+
+                choose(currNum + 1);
+
+                ans.remove(ans.size() - 1);
+                visited[i] = false;
+            }
+        }
+        return;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        // Please write your code here.
+        choose(0);
+    }
+}
