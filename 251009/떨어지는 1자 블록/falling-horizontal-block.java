edc6119c6f4ea39785d858501c213be9c4ee4424
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-10-09 20:03:51 +0900

    Title: 떨어지는 1자 블록, Time: 122ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251009/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static int n, m, k;
+    public static int[][] grid = new int[101][101];
+    public static void gravity(){
+        int min = Integer.MAX_VALUE;
+        for(int i = k-1; i < k + m-1; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[j][i] == 1 && min > j){
+                    min = j - 1;
+                }
+            }
+        }
+        for(int i = k-1; i < k + m-1; i++){
+            grid[min][i] = 1;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        // Please write your code here.
+
+        gravity();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(grid[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
