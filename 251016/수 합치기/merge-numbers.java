338516ad6b8dc87dd4a4f16fb4f5fde787419545
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-10-16 18:00:30 +0900

    Title: 수 합치기, Time: 934ms, Memory: 36MB, Status: Passed - Codetree

--- 251016/수 합치기/merge-numbers.java
+++ 251016/수 합치기/merge-numbers.java
@@ -1,22 +1,23 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        int v = 0;
+        PriorityQueue<Long> pq = new PriorityQueue<>(); // 최소 힙
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            pq.add((long) sc.nextInt()); // 입력을 long으로 변환
         }
-        // Please write your code here.
-        for(int i = 0; i < n - 1; i++){
-            Arrays.sort(arr);
-            v += arr[0]+arr[1];
-            arr[0] = arr[0] + arr[1];
-            arr[1] = Integer.MAX_VALUE;
+        
+        long totalCost = 0;
+        while (pq.size() > 1) { // 1개 남을 때까지
+            long a = pq.poll(); // 가장 작은 수
+            long b = pq.poll(); // 다음 작은 수
+            long sum = a + b;
+            totalCost += sum;   // 비용 누적
+            pq.add(sum);        // 합친 값 다시 넣기
         }
-
-        System.out.print(v);
-
+        
+        System.out.println(totalCost);
     }
 }
