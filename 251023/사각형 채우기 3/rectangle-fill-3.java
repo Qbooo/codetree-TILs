307e0c207b2bf626c7dc95970989ea708b143ca4
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-10-23 01:23:31 +0900

    Title: 사각형 채우기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251023/사각형 채우기 3/rectangle-fill-3.java
+++ 251023/사각형 채우기 3/rectangle-fill-3.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class Main {
-    public static final int MAX = 1000;
-    public static long[] dp = new long[MAX+1];
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        // Please write your code here.
-        dp[1] = 2;
-        dp[2] = 7;
-
-        for(int i = 3; i < n+1; i++){
-            dp[i] = ((dp[i-1]  * 2L + dp[i-2]  * 4L)%1000000007);
-        }
-
-        System.out.print(dp[n]);
-    }
+    public static final int MAX = 1000;
+    public static final int MOD = 1000000007; // 상수로 정의
+    public static int[] dp = new int[MAX+1];
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       
+        dp[1] = 2;
+        dp[2] = 7;
+        for(int i = 3; i <= n; i++){
+            // ✅ 모든 연산 후 % MOD
+            dp[i] = (int)((dp[i-1] * 2L + dp[i-2] * 4L) % MOD);
+        }
+        System.out.print(dp[n]);
+    }
 }
