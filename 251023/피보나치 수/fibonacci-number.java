ce7e99c8896e3c66d28d66f530fab60f7563b78a
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-10-23 00:34:13 +0900

    Title: 피보나치 수, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

--- 251023/피보나치 수/fibonacci-number.java
+++ 251023/피보나치 수/fibonacci-number.java
@@ -4,14 +4,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // Please write your code here.
+
         int[] dp = new int[n];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2; i < n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        if(n == 1){
+            System.out.print(1);
+        }else if(n == 2){
+            System.out.print(1);
+        }else{
+            dp[0] = 1;
+            dp[1] = 1;
+            for(int i = 2; i < n; i++){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+
+            System.out.println(dp[n-1]);
         }
 
-        System.out.println(dp[n-1]);
 
     }
 }
