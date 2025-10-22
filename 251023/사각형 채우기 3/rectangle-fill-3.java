de52a2a29441218434b7f2db5361dd93e27477c8
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-10-23 01:18:00 +0900

    Title: 사각형 채우기 3, Time: 121ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251023/사각형 채우기 3/rectangle-fill-3.java
+++ 251023/사각형 채우기 3/rectangle-fill-3.java
@@ -11,7 +11,7 @@ public class Main {
         dp[2] = 7;
 
         for(int i = 3; i < n+1; i++){
-            dp[i] = (dp[i-1] * 2 + dp[i-2] * 4)%1000000007;
+            dp[i] = (dp[i-1] %1000000007 * 2 + dp[i-2] %1000000007 * 4);
         }
 
         System.out.print(dp[n]);
