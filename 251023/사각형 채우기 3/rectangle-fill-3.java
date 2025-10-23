d3cb0b0f1c95162675fb53122e13c986f8e85f35
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-10-23 15:20:46 +0900

    Title: 사각형 채우기 3, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251023/사각형 채우기 3/rectangle-fill-3.java
+++ 251023/사각형 채우기 3/rectangle-fill-3.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 public class Main {
     public static final int MAX = 1000;
+    public static final int MOD = 1000000007;
     public static long[] dp = new long[MAX+1];
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
+        dp[0] = 1;
         dp[1] = 2;
-        dp[2] = 7;
 
-        for(int i = 3; i <= n; i++){
-            dp[i] = ((dp[i-1] * 2L + dp[i-2] * 4L) % 1000000007L);
-            //           ↑↑-----------------------------↑↑
-            //           괄호 필수!
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i-1]*2 + dp[i-2]*3) % MOD;
+            for(int j = i - 3; j >= 0; j--){
+                dp[i] = (dp[i] + dp[j] * 2) % MOD;
+            }
         }
 
         System.out.print(dp[n]);
