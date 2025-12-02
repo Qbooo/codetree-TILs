1693793434c13f1d24b637e13709cbd1687a1e2e
Kyubo Sim <70837945+Qbooo@users.noreply.github.com>
2025-12-02 15:25:56 +0900

    Title: 운행 되고 있는 시간, Time: 153ms, Memory: 10MB, Status: Passed - Codetree

--- 251202/운행 되고 있는 시간/hours-in-service.java
+++ 251202/운행 되고 있는 시간/hours-in-service.java
@@ -1,43 +1,75 @@
-import java.util.Scanner;
+import java.util.*;
+
+class Event implements Comparable<Event> {
+    int time;
+    int type;   // 0: 시작, 1: 끝
+    int idx;    // 직원 번호 (-1이면 전체 이벤트)
+
+    Event(int time, int type, int idx) {
+        this.time = time;
+        this.type = type;
+        this.idx = idx;
+    }
+
+    @Override
+    public int compareTo(Event o) {
+        if (this.time != o.time) return Integer.compare(this.time, o.time);
+        // 같은 시간이면 시작(0)이 끝(1)보다 먼저 와야 함
+        return Integer.compare(this.type, o.type);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] A = new int[N];
-        int[] B = new int[N];
+        int[][] intervals = new int[N][2];
+        List<Event> events = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
-            B[i] = sc.nextInt();
-        }
-        // Please write your code here.
-        int max = 0;
-        for(int i = 0; i < N; i++){
-            int cnt = 0;
-            int[] s = new int[1001];
-            for(int j = 0; j < i; j++){
-                for(int k = A[j]; k < B[j]; k++){
-                    s[k]++;
-                }
-            }
-            for(int j = i + 1; j < N; j++){
-                for(int k = A[j]; k < B[j]; k++){
-                    s[k]++;
-                }
-            }
-
-            for(int j = 0; j < 1001; j++){
-                if(s[j] > 0){
-                    cnt++;
-                }
-            }
-
-            if(max < cnt){
-                max = cnt;
-            }
-
-
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            intervals[i][0] = a;
+            intervals[i][1] = b;
+            events.add(new Event(a, 0, i));  // 시작
+            events.add(new Event(b, 1, i));  // 끝
         }
 
-        System.out.print(max);
+        Collections.sort(events);
+
+        // 1. 전체 union 길이 계산 + 각 시간대에 몇 명이 일하는지 기록
+        int[] cover = new int[1001];  // cover[t] = t시각에 일하는 사람 수
+        int active = 0;
+        int prev = -1;
+        int totalUnion = 0;
+
+        for (Event e : events) {
+            if (prev != -1 && e.time != prev && active > 0) {
+                totalUnion += e.time - prev;
+                for (int t = prev; t < e.time; t++) {
+                    cover[t] = active;
+                }
+            }
+            if (e.type == 0) active++;
+            else active--;
+            prev = e.time;
+        }
+
+        // 2. 각 직원 i를 제외했을 때 사라지는 길이 = i가 단독으로 커버한 구간 길이
+        int minLost = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int a = intervals[i][0];
+            int b = intervals[i][1];
+            int lost = 0;
+            for (int t = a; t < b; t++) {
+                if (cover[t] == 1) {  // 이 시간에 나만 일하고 있었다!
+                    lost++;
+                }
+            }
+            minLost = Math.min(minLost, lost);
+        }
+
+        System.out.println(totalUnion - minLost);
     }
 }
